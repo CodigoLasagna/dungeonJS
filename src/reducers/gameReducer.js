199546fde46b0017230db8cab96e8f4beb87961b
Hanck Edd <hanckeddison@gmail.com>
2024-09-26 19:58:52 -0700

    walls

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,21 +1,22 @@
 const initialState = {
-	playerPosition: {x : 2, y : 2},
+	playerPosition: {x : 1, y : 1},
 	dungeon: [
-		['.', '.', '.', '.', '.'],
-		['.', '.', '.', '.', '.'],
-		['.', '.', '@', '.', '.'],
-		['.', '.', '.', '.', '.'],
-		['.', '.', '.', '.', '.'],
+		['#', '#', '#', '#', '#'],
+		['#', '.', '.', '.', '#'],
+		['#', '.', '#', '.', '#'],
+		['#', '.', '.', '.', '#'],
+		['#', '#', '#', '#', '#'],
 	], //Plantilla sencilla de un mapa 5x5
 };
 
 const gameReducer = (state = initialState, action) => {
 	const { playerPosition, dungeon } = state;
+	const { x, y } = playerPosition;
+	let newPos = { x, y };
+	let validMove = true;
 	switch (action.type) {
 		case 'MOVE_PLAYER':
-			const { x, y } = playerPosition;
-			let newPos = { x, y };
-			let validMove = true;
+			const prevPos = { ...newPos };
 			
 		// aquí se designa como funciona el movimiento
 		if (action.payload === 'UP' && y > 0) newPos.y--;
@@ -23,6 +24,11 @@ const gameReducer = (state = initialState, action) => {
 		else if (action.payload === 'LEFT' && x > 0) newPos.x--;
 		else if (action.payload === 'RIGHT' && x < dungeon[0].length - 1) newPos.x++;
 		else validMove = false
+
+		if (dungeon[newPos.y][newPos.x] === '#'){
+			validMove = false;
+			newPos = prevPos;
+		}
 		
 		// se dibuja el jugador
 		const newDungeon = dungeon.map((row, rowIndex) =>
